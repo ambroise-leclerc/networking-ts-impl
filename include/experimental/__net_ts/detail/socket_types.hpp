ee633b29fc5e309f1aff8dce014b628670baed79
Ambroise Leclerc <github.com@physigenics.com>
2023-04-11 12:31:10 +0200

    error: signed conversion from ‘unsigned int’ to ‘int’ changes value from ‘2769289216’ to ‘-1525678080’

--- include/experimental/__net_ts/detail/socket_types.hpp
+++ include/experimental/__net_ts/detail/socket_types.hpp
@@ -407,7 +407,7 @@ const int max_iov_len = IOV_MAX;
 const int max_iov_len = 16;
 # endif
 #endif
-const int custom_socket_option_level = 0xA5100000;
+const auto custom_socket_option_level = 0xA5100000;
 const int enable_connection_aborted_option = 1;
 const int always_fail_option = 2;
 
